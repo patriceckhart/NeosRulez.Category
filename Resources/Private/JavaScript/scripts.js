8e2a06d2f9bd25793fddc73f3f356d4935165fbb
Patric Eckhart <mail@patriceckhart.com>
2020-11-04 11:02:46 +0100

    fix: click on any element bug

--- Resources/Private/JavaScript/scripts.js
+++ Resources/Private/JavaScript/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     if($('.filtered').length) {
         var mixer = mixitup('.filtered', {
+            selectors: {
+                control: '.filter-btn'
+            },
+            classNames: {
+                block: '',
+                elementFilter: ''
+            },
             animation: {
                 effects: 'fade scale(0.5)'
             }
